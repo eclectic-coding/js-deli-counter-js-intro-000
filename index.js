50d9528934bd5c7382bdf82fd25f9b5b651cb03b
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-07 15:23:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(num, name) {
   num.push(name)
   
-  return num.length(`Welcome ${name}. You are number ${num} in line."`)
+  return (`Welcome ${name}. You are number ${num} in line."`)
   
 }
